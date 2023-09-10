49d36a3e7ef6b48f7c0b42a1017e234a0d7d827e
Clemens <git@cylancer.net>
2023-09-10 16:44:28 +0200

    Development (#9)
    * 2.1.3 :: Add documentarion / change this icons.
    
    * Remove news extension dependency
    
    * Removes the news extension dependecy (Removes the newsletter task. You
    can use the scheduler task cy_newsletter.)
    
    * Update bootstrap dependency
    
    * Update bootstrap_package dependency: Allows the version 13.
    
    * Update readme.md
    
    * Update readme.md
    
    * update readme.md
    
    * Repair the plugin configuration / registry.
    
    ---------
    
    Co-authored-by: Clemens Gogoljn <service@cylancer.net>

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,20 +1,63 @@
 <?php
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['usertools_changeemailform'] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    // plugin signature: <extension key without underscores> '_' <plugin name in lowercase>
-    'usertools_changeemailform', 
-    // Flexform configuration schema file
-    'FILE:EXT:usertools/Configuration/FlexForms/ChangeEmail.xml');
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['usertools_listusers'] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    // plugin signature: <extension key without underscores> '_' <plugin name in lowercase>
-    'usertools_listusers', 
-    // Flexform configuration schema file
-    'FILE:EXT:usertools/Configuration/FlexForms/ListUsers.xml');
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['usertools_editprofile'] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    // plugin signature: <extension key without underscores> '_' <plugin name in lowercase>
-    'usertools_editprofile', 
-    // Flexform configuration schema file
-    'FILE:EXT:usertools/Configuration/FlexForms/EditProfile.xml');
-?>
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+defined('TYPO3') or die();
+
+(static function (): void{
+
+
+    ExtensionUtility::registerPlugin(
+        'Usertools',
+        'Editprofile',
+        'LLL:EXT:usertools/Resources/Private/Language/locallang_be_editProfile.xlf:plugin.name'
+    );
+
+    ExtensionUtility::registerPlugin(
+        'Usertools',
+        'Listusers',
+        'LLL:EXT:usertools/Resources/Private/Language/locallang_be_listUsers.xlf:plugin.name'
+    );
+
+    ExtensionUtility::registerPlugin(
+        'Usertools',
+        'Changeemailform',
+        'LLL:EXT:usertools/Resources/Private/Language/locallang_be_changeEmail.xlf:plugin.name'
+    );
+
+    ExtensionUtility::registerPlugin(
+        'Usertools',
+        'Confirmnewemail',
+        'LLL:EXT:usertools/Resources/Private/Language/locallang_be_confirmNewEmail.xlf:plugin.name'
+    );
+
+    ExtensionUtility::registerPlugin(
+        'Usertools',
+        'Changeuserpassword',
+        'LLL:EXT:usertools/Resources/Private/Language/locallang_be_changePassword.xlf:plugin.name'
+    );
+
+
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['usertools_changeemailform'] = 'pi_flexform';
+    ExtensionManagementUtility::addPiFlexFormValue(
+        // plugin signature: <extension key without underscores> '_' <plugin name in lowercase>
+        'usertools_changeemailform',
+        // Flexform configuration schema file
+        'FILE:EXT:usertools/Configuration/FlexForms/ChangeEmail.xml'
+    );
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['usertools_listusers'] = 'pi_flexform';
+    ExtensionManagementUtility::addPiFlexFormValue(
+        // plugin signature: <extension key without underscores> '_' <plugin name in lowercase>
+        'usertools_listusers',
+        // Flexform configuration schema file
+        'FILE:EXT:usertools/Configuration/FlexForms/ListUsers.xml'
+    );
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['usertools_editprofile'] = 'pi_flexform';
+    ExtensionManagementUtility::addPiFlexFormValue(
+        // plugin signature: <extension key without underscores> '_' <plugin name in lowercase>
+        'usertools_editprofile',
+        // Flexform configuration schema file
+        'FILE:EXT:usertools/Configuration/FlexForms/EditProfile.xml'
+    );
+
+})();
